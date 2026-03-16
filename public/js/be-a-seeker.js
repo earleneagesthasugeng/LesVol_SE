6c5c5ec697a431a283cb4d14a1881d8df3199179
Adriel Agustinus Jie <adrieljie2708@gmail.com>
2026-03-16 18:50:14 +0800

    tambah modal di be-a-seeker

--- public/js/be-a-seeker.js
+++ public/js/be-a-seeker.js
@@ -4,18 +4,15 @@ const uploadTrigger = document.getElementById('upload-trigger');
 const preview = document.getElementById('image-preview');
 const placeholder = document.getElementById('upload-placeholder');
 
-// 1. Klik area box atau tombol upload untuk buka file manager
 dropzone.addEventListener('click', () => fileInput.click());
 uploadTrigger.addEventListener('click', () => {
     if(fileInput.files.length === 0) {
         fileInput.click();
     } else {
-        alert('File siap diunggah ke server!');
-        // Di sini kamu bisa tambahkan logika upload ke database
+        openModal('modal-register-success');
     }
 });
 
-// 2. Logika untuk menampilkan gambar setelah dipilih
 fileInput.addEventListener('change', function() {
   const file = this.files[0];
   if (file) {
@@ -23,9 +20,9 @@ fileInput.addEventListener('change', function() {
     
     reader.onload = function(e) {
       preview.src = e.target.result;
-      preview.style.display = 'block';      // Tampilkan preview
-      placeholder.style.display = 'none';   // Sembunyikan ikon upload
-      dropzone.style.borderStyle = 'solid'; // Ubah border jadi solid
+      preview.style.display = 'block'; 
+      placeholder.style.display = 'none';   
+      dropzone.style.borderStyle = 'solid'; 
     }
     
     reader.readAsDataURL(file);
